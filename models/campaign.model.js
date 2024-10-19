e68524ab7136dcce9a787cec811c927b552d5853
reyhanrab <C0897171@mylambton.ca>
2024-10-19 18:04:18 -0400

    Added Campaign Model and Missing Routes

---
+++ models/campaign.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const campaignSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  targetAmount: {
+    type: Number,
+    required: true,
+  },
+  raisedAmount: {
+    type: Number,
+    default: 0,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  }
+});
+
+const Campaign = mongoose.model("campaign", campaignSchema);
+
+export default Campaign;
